e690e8358ba91821bdf54c508ca8f51bb540afe3
pyld <lidongone@qq.com>
2015-11-15 02:05:41 +0800

    1

--- trequests.py
+++ trequests.py
@@ -49,41 +49,53 @@ class Pool():
     '''
 Obviously, use it like :
 from trequests import Pool
+
 requests = Pool(30)
 ...
 then use requests.get/post/put/head/delete/ as usual.
 so, this does not support Session...
     '''
 
-    def __init__(self, poolnum):
+    def __init__(self, poolnum, session=None):
         self.poolnum = poolnum
+        self.session = session
 
     def get(self, url, **kws):
         @threads(self.poolnum)
         def get1(url, **kws):
+            if self.session:
+                return self.session.get(url, **kws)
             return requests.get(url, **kws)
         return get1(url, **kws)
 
     def post(self, url, **kws):
         @threads(self.poolnum)
         def post1(url, **kws):
+            if self.session:
+                return self.session.post(url, **kws)
             return requests.post(url, **kws)
         return post1(url, **kws)
 
     def delete(self, url, **kws):
         @threads(self.poolnum)
         def delete1(url, **kws):
+            if self.session:
+                return self.session.delete(url, **kws)
             return requests.delete(url, **kws)
         return delete1(url, **kws)
 
     def put(self, url, **kws):
         @threads(self.poolnum)
         def put1(url, **kws):
+            if self.session:
+                return self.session.put(url, **kws)
             return requests.put(url, **kws)
         return put1(url, **kws)
 
     def head(self, url, **kws):
         @threads(self.poolnum)
         def head1(url, **kws):
+            if self.session:
+                return self.session.head(url, **kws)
             return requests.head(url, **kws)
         return head1(url, **kws)
