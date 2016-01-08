24f57b3abc19ae6492c11081d853b98c7bfe31ce
ClericPy <1>
2016-01-08 17:12:00 +0800

    add delay time for tPool

--- torequests.py
+++ torequests.py
@@ -4,6 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 from multiprocessing.dummy import Pool as multiPool
 import time
 
+
 class Tomorrow():
 
     def __init__(self, future, timeout1):
