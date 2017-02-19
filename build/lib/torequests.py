a614c525b26d684da574e471e99f8182edb58a9c
ClericPy <lidongone@qq.com>
2017-02-20 01:46:20 +0800

    update to 3.0.0

--- build/lib/torequests.py
+++ build/lib/torequests.py
@@ -8,40 +8,50 @@ import requests
 from requests import Session
 
 
-class AsyncPool(ThreadPoolExecutor):
+class Pool(ThreadPoolExecutor):
 
-    def __init__(self, num, timeout, timeout_return):
-        super(AsyncPool, self).__init__(num)
+    '''timeout_return while .x called and timeout.'''
+
+    def __init__(self, n=None, timeout=None, timeout_return=None):
+        if n is None and (not isinstance(range,type)):
+            # python2 n!=None
+            n = 20
+        super(Pool, self).__init__(n)
         self.timeout = timeout
         self.timeout_return = timeout_return
 
-    def async_func(self):
-        def decorator(f):
-            @wraps(f)
-            def wrapped(*args, **kwargs):
-                return self.submit(f, *args, **kwargs)
-            return wrapped
-        return decorator
+    def async_func(self, f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            return self.submit(f, *args, **kwargs)
+        return wrapped
 
-    def submit(self, fn, *args, **kwargs):
+    def submit(self, func, *args, **kwargs):
         with self._shutdown_lock:
             if self._shutdown:
-                raise RuntimeError('cannot schedule new futures after shutdown')
-            f = NewFuture(self.timeout, self.timeout_return)
-            w = _WorkItem(f, fn, args, kwargs)
+                raise RuntimeError(
+                    'cannot schedule new futures after shutdown')
+            future = NewFuture(
+                self.timeout, self.timeout_return, args, kwargs)
+            w = _WorkItem(future, func, args, kwargs)
             self._work_queue.put(w)
             self._adjust_thread_count()
-            return f
+            return future
 
 
 class NewFuture(Future):
 
-    """add .x (property) and timeout/timeout_return args for original Future"""
+    """add .x (property) and timeout/timeout_return args for original Future
+    timeout_return function only can be called while .x attribute called and raise TimeoutError.
+    WARNING: Future thread will not stop running until function finished or pid killed.
+    """
 
-    def __init__(self, timeout=None, timeout_return='TimeoutError'):
+    def __init__(self, timeout=None, timeout_return=None, args=(), kwargs={}):
         super(NewFuture, self).__init__()
         self._timeout = timeout
         self._timeout_return = timeout_return
+        self._args = args
+        self._kwargs = kwargs
 
     def __getattr__(self, name):
         result = self.result(self._timeout)
@@ -52,24 +62,48 @@ class NewFuture(Future):
         try:
             return self.result(self._timeout)
         except TimeoutError:
+            if not self._timeout_return:
+                return 'TimeoutError: %s, %s' % (self._args, self._kwargs)
+            if hasattr(self._timeout_return, '__call__'):
+                return self._timeout_return(*self._args, **self._kwargs)
             return self._timeout_return
 
 
-def Async(f, n=30, timeout=None, timeout_return='TimeoutError'):
-    '''Here "Async" is not a class object, upper "A" only be used to differ from keyword "async" since python3.5+.'''
-    return AsyncPool(n, timeout, timeout_return).async_func()(f)
+def Async(f, n=None, timeout=None, timeout_return=None):
+    '''Here "Async" is not a class object, upper "A" only be used to differ
+        from keyword "async" since python3.5+.
+        Args:
+        f : Async the function object, f.
+        n=None: (os.cpu_count() or 1) * 5, The maximum number of threads that 
+            can be used to execute the given calls.
+        timeout=None: Future.x will wait for `timeout` seconds for the function's 
+            result,  or return timeout_return(*args, **kwargs). 
+            WARN: Future thread will not stop running until function finished or pid killed.
+        timeout_return=None: Call Future.x after timeout, if timeout_return is 
+            not true, return 'TimeoutError: %s, %s' % (self._args, self._kwargs) if timeout_return has __call__ attr, return timeout_return(*args, **kwargs) otherwise, return timeout_return itself.
 
-# async = Async  # This has be abandoned even before Python3.7 released.
+        Read more from model __doc__.
+
+'''
+    return Pool(n, timeout, timeout_return).async_func(f)
 
 
-def threads(n=30, timeout=None, timeout_return='TimeoutError'):
-    return AsyncPool(n, timeout, timeout_return).async_func()
+def threads(n=None, timeout=None, timeout_return=None):
+    '''Args:
+        f : Async the function object, f.
+        n=None: (os.cpu_count() or 1) * 5, The maximum number of threads that can be used to execute the given calls.
+        timeout=None: Future.x will wait for `timeout` seconds for the function's result,  or return timeout_return(*args, **kwargs). WARN: Future thread will not stop running until function finished or pid killed.
+        timeout_return=None: Call Future.x after timeout, if timeout_return is not true, return 'TimeoutError: %s, %s' % (self._args, self._kwargs) if timeout_return has __call__ attr, return timeout_return(*args, **kwargs) otherwise, return timeout_return itself.
+
+        Read more from model __doc__.
+'''
+    return Pool(n, timeout, timeout_return).async_func
 
 
-def get_by_time(fs, timeout=None):
+def get_by_time(new_futures, timeout=None):
     '''Return a generator'''
     try:
-        for i in as_completed(fs, timeout=timeout):
+        for i in as_completed(new_futures, timeout=timeout):
             yield i.x
     except Exception as e:
         yield e
@@ -77,96 +111,67 @@ def get_by_time(fs, timeout=None):
 
 class tPool():
 
-    def __init__(self, num=30, session=None):
-        self.num = num
-        self.session = session if session else requests
+    def __init__(self, n=None, session=None, timeout=None, timeout_return=None):
+        self.pool = Pool(n, timeout, timeout_return)
+        self.n = n
+        self.session = session if session else requests.Session()
 
-    def get(self, url, retry=0, retrylog=False, logging=None, delay=0, fail_return=False, **kws):
-        @threads(self.num)
-        def get1(url, **kws):
-            for _ in range(retry+1):
-                try:
-                    time.sleep(delay)
-                    ss = self.session.get(url, **kws)
-                    if logging:
-                        print(url, logging)
-                    return ss
-                except Exception as e:
-                    if retrylog:
-                        print('Retry %s for the %s time,Exception:' %
-                              (url, _+1), e)
-                    continue
-            return fail_return
-        return get1(url, **kws)
+    def close(self, wait=True):
+        self.session.close()
+        self.pool.shutdown(wait=wait)
 
-    def post(self, url, retry=0, retrylog=False, logging=None, delay=0, fail_return=False, **kws):
-        @threads(self.num)
-        def post1(url, **kws):
-            for _ in range(retry+1):
-                try:
-                    time.sleep(delay)
-                    ss = self.session.post(url, **kws)
-                    if logging:
-                        print(url, logging)
-                    return ss
-                except Exception as e:
-                    if retrylog:
-                        print('Retry %s for the %s time,Exception:' %
-                              (url, _+1), e)
-                    continue
-            return fail_return
-        return post1(url, **kws)
+    def __enter__(self):
+        return self
 
-    def delete(self, url, retry=0, retrylog=False, logging=None, delay=0, fail_return=False, **kws):
-        @threads(self.num)
-        def delete1(url, **kws):
-            for _ in range(retry+1):
-                try:
-                    time.sleep(delay)
-                    ss = self.session.delete(url, **kws)
-                    if logging:
-                        print(url, logging)
-                    return ss
-                except Exception as e:
-                    if retrylog:
-                        print('Retry %s for the %s time,Exception:' %
-                              (url, _+1), e)
-                    continue
-            return fail_return
-        return delete1(url, **kws)
+    def __exit__(self, *args):
+        self.close()
 
-    def put(self, url, retry=0, retrylog=False, logging=None, delay=0, fail_return=False, **kws):
-        @threads(self.num)
-        def put1(url, **kws):
-            for _ in range(retry+1):
-                try:
-                    time.sleep(delay)
-                    ss = self.session.put(url, **kws)
-                    if logging:
-                        print(url, logging)
-                    return ss
-                except Exception as e:
-                    if retrylog:
-                        print('Retry %s for the %s time,Exception:' %
-                              (url, _+1), e)
-                    continue
-            return fail_return
-        return put1(url, **kws)
+    def request(self, url, mode, retry=0, retrylog=False, logging=False,
+                delay=0, fail_return=False, **kwargs):
+        for _ in range(retry+1):
+            try:
+                time.sleep(delay)
+                resp = self.session.request(mode, url, **kwargs)
+                if logging:
+                    print('%s done, %s' % (url, kwargs))
+                return resp
+            except Exception as e:
+                if retrylog:
+                    print('Retry %s for the %s time, Exception: %s . kwargs= %s' %
+                              (url, _+1, e, kwargs))
+                continue
+        return fail_return
 
-    def head(self, url, retry=0, retrylog=False, logging=None, delay=0, fail_return=False, **kws):
-        @threads(self.num)
-        def head1(url, **kws):
-            for _ in range(retry+1):
-                try:
-                    time.sleep(delay)
-                    ss = self.session.head(url, **kws)
-                    if logging:
-                        print(url, logging)
-                    return ss
-                except Exception as e:
-                    if retrylog:
-                        print('Retry %s for the %s time,Exception:' %
-                              (url, _+1), e)
-                    continue
-            return fail_return
-        return head1(url, **kws)
+    def get(self, url, **kwargs):
+        '''retry=0, retrylog=False, logging=None, delay=0, fail_return=False
+            retry: retry time for exception
+            logging: set logging=True will show url and kwargs when successful.
+            retrylog: show error while catching exception after retrying.
+            delay: time.sleep(delay) before request
+            fail_return: return after retry arg `retry` times but fail
+                , it may be a function(url, **args) or other.
+
+        '''
+        print(1)
+        return self.pool.submit(self.request, url, 'get', **kwargs)
+
+    def post(self, url, **kwargs):
+        '''retry=0, retrylog=False, logging=None, delay=0, fail_return=False
+            retry: retry time for exception
+            logging: set logging=True will show url and kwargs when successful.
+            retrylog: show error while catching exception after retrying.
+            delay: time.sleep(delay) before request
+            fail_return: return after retry arg `retry` times but fail
+                , it may be a function(url, **args) or other.
+
+        '''
+        return self.pool.submit(self.request, url, 'post', **kwargs)
+
+    def delete(self, url, **kwargs):
+        return self.pool.submit(self.request, url, 'delete', **kwargs)
+
+    def put(self, url, **kwargs):
+        return self.pool.submit(self.request, url, 'put', **kwargs)
+
+    def head(self, url, **kwargs):
+        return self.pool.submit(self.request, url, 'head', **kwargs)
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='torequests',
-    version='2.2.1',
+    version='3.0.0',
     keywords=('requests tomorrow async multi-thread concurrent.futures asynchronous'),
     description='Asynchronous requests. Using concurrent.futures to make requests async(like Tomorrow), and some functions to make async-coding easy. read more :https://github.com/ClericPy/torequests ',
     license='MIT License',
     install_requires=['requests','futures'],
     py_modules=['torequests'],
     author='ClericPy',
-    author_email='pythonld@163.com',
+    author_email='clericpy@gmail.com',
     url='https://github.com/ClericPy/torequests',
     packages=find_packages(),
     platforms='any',
