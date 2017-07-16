e68b647cb7423e9410b29b1f1f53b938a7973e20
clericpy <lidongone@qq.com>
2017-07-17 04:03:41 +0800

    wip processes failed again

--- torequests/process.py
+++ torequests/process.py
@@ -2,62 +2,60 @@
 # python2 requires: pip install futures
 import time
 import os
-from concurrent.futures import ProcessPoolExecutor
-from concurrent.futures.process import _WorkItem
-
-from concurrent.futures._base import Future, TimeoutError
+from multiprocessing.pool import Pool, ApplyResult
+from multiprocessing.context import TimeoutError
 from functools import wraps
 from .utils import FailureException
+from .main import Async
 
 
-class Pool(ProcessPoolExecutor):
+class ProcessPool(Pool):
     '''
-    add async_func(function decorator) for submitting called-function into Pool obj.
+    add async_func(function decorator) for submitting called-function into ProcessPool obj.
     '''
 
     def __init__(self, n=None, timeout=None):
-        super(Pool, self).__init__(n)
+        super(ProcessPool, self).__init__(n)
         self._timeout = timeout
+        self._apply = Async(self.apply, n)
 
     def async_func(self, function):
         @wraps(function)
         def wrapped(*args, **kwargs):
-            return self.submit(function, *args, **kwargs)
+            return self._apply(function, args, kwargs)
         return wrapped
 
     def close(self, wait=True):
         self.shutdown(wait=wait)
 
-    def submit(self, func, *args, **kwargs):
-        '''self.submit(function,arg1,arg2,arg3=3)'''
+    def apply(self, func, args=(), kwds={}):
+        '''
+        Equivalent of `func(*args, **kwds)`.
+        '''
+        assert self._state == 0
+        return self.apply_async(func, args, kwds).x
 
-        with self._shutdown_lock:
-            # now work at python2.x
-            # if self._broken:
-                # raise BrokenProcessPool('A child process terminated '
-                                        # 'abruptly, the process pool is not usable anymore')
-            if self._shutdown_thread:
-                raise RuntimeError(
-                    'cannot schedule new futures after shutdown')
-            future = NewFuture(self._timeout, args, kwargs)
-            w = _WorkItem(future, func, args, kwargs)
-            self._pending_work_items[self._queue_count] = w
-            self._work_ids.put(self._queue_count)
-            self._queue_count += 1
-            # Wake up queue management thread
-            self._result_queue.put(None)
-            self._start_queue_management_thread()
-            return future
+    def apply_async(self, func, args=(), kwds={}, callback=None,
+                    error_callback=None):
+        '''
+        Asynchronous version of `apply()` method.
+        '''
+        if self._state != 0:
+            raise ValueError("Pool not running")
+        result = NewApplyResult(self._cache, callback, error_callback,
+                                args=args, kwargs=kwds, timeout=self._timeout)
+        self._taskqueue.put(([(result._job, None, func, args, kwds)], None))
+        return result
 
 
-class NewFuture(Future):
+class NewApplyResult(ApplyResult):
 
     """add .x (property) and timeout args for original Future class
     WARNING: Future process will be killed if timeout.
     """
 
-    def __init__(self, timeout=None, args=None, kwargs=None):
-        super(NewFuture, self).__init__()
+    def __init__(self, cache, callback, error_callback, timeout=None, args=None, kwargs=None):
+        super(NewApplyResult, self).__init__(cache, callback, error_callback)
         self._timeout = timeout
         self._args = args or ()
         self._kwargs = kwargs or {}
@@ -71,15 +69,17 @@ class NewFuture(Future):
     @property
     def x(self):
         try:
-            return self.result(self._timeout)
-        except TimeoutError as err:
+            return self.get(self._timeout)
+        except Exception as err:
             err.args = (self._args, self._kwargs)
             return FailureException(err)
 
 
 def Daemon(f, n=None, timeout=None):
-    return Pool(n, timeout).async_func(f)
+    pool = ProcessPool(n, timeout)
+    f.pool = pool
+    return pool.async_func(f)
 
-# block coding for _ForkingPickler.dump exception: it's not the same object as
+# block coding for _ForkingPickler.dump PicklingError: it's not the same object as
 # def processes(n=None, timeout=None):
-#     return Pool(n, timeout).async_func
+#     return ProcessPool(n, timeout).async_func
