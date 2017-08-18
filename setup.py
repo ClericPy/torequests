ce58d3823ddfa395d6e003a0a0aae32ac80f05ec
clericpy <lidongone@qq.com>
2017-08-19 02:20:55 +0800

    python3 add uvloop

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #! coding:utf-8
 from setuptools import setup, find_packages
+import sys
+import platform
 '''
 linux:
 python3 setup.py bdist_wheel;python2 setup.py bdist_wheel;twine upload dist/*
@@ -7,13 +9,24 @@ win32:
 python3 setup.py bdist_wheel&python2 setup.py bdist_wheel&twine upload dist/*
 '''
 
+
+py_version = sys.version_info
+install_requires = ['requests']
+if py_version.major == 2:
+    install_requires.append('futures')
+if py_version.minor >= 5:
+    install_requires.append('aiohttp')
+    if  'Windows' not in platform.system():
+        install_requires.append('uvloop')
+
+# print(install_requires)
 setup(
     name='torequests',
     version='4.4.2',
     keywords=('requests tomorrow async multi-thread aiohttp asyncio uvloop futures asynchronous'),
     description='Asynchronous requests. Using concurrent.futures to make requests asynchronous, and some functions to make async-coding easy. Besides, add aiohttp+uvloop to enjoy python3.5+ coroutine. Read more: https://github.com/ClericPy/torequests.',
     license='MIT License',
-    install_requires=['requests','futures'],
+    install_requires=install_requires,
     py_modules=['torequests'],
     author='ClericPy',
     author_email='clericpy@gmail.com',
