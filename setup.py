f414536542f9749efa19e83a974b9ea905daf75e
clericpy <lidongone@qq.com>
2017-09-06 11:56:11 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -14,15 +14,14 @@ py_version = sys.version_info
 install_requires = ['requests']
 if py_version.major == 2:
     install_requires.append('futures')
-if py_version.minor >= 5:
+if py_version.major == 3 and py_version.minor >= 5:
     install_requires.append('aiohttp')
-    if  'Windows' not in platform.system():
-        install_requires.append('uvloop')
+    # if  'Windows' not in platform.system():
+    #     install_requires.append('uvloop')
 
-# print(install_requires)
 setup(
     name='torequests',
-    version='4.4.5',
+    version='4.4.6',
     keywords=('requests tomorrow async multi-thread aiohttp asyncio uvloop futures asynchronous'),
     description='Asynchronous requests. Using concurrent.futures to make requests asynchronous, and some functions to make async-coding easy. Besides, add aiohttp+uvloop to enjoy python3.5+ coroutine. Read more: https://github.com/ClericPy/torequests.',
     license='MIT License',
