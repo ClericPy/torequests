e642f273ade5ff7df42546cb72e6dbe05c435fb0
clericpy <lidongone@qq.com>
2018-03-05 00:17:59 +0800

    update 4.5.11

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if py_version.major == 3 and py_version.minor >= 5:
 
 setup(
     name='torequests',
-    version='4.5.10',
+    version='4.5.11',
     keywords=(
         'requests async multi-thread aiohttp asyncio uvloop asynchronous'),
     description=
