44025ed450da1a3e1e61d93c5b22ff9bb4d637cb
clericpy <lidongone@qq.com>
2018-03-19 02:31:31 +0800

    update

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ author = 'Clericpy'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '4.6.5'
+release = '4.6.6'
 
 
 # -- General configuration ---------------------------------------------------
