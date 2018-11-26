becced72894f01db4de9e5a6c04e0594c526f4c5
clericpy <lidongone@qq.com>
2018-11-26 23:07:09 +0800

    add __version__

--- torequests/__init__.py
+++ torequests/__init__.py
@@ -1,7 +1,14 @@
 #! coding: utf-8
 from .main import *
 
+__version__ = "4.7.17"
 __all__ = [
-    "Pool", "ProcessPool", "NewFuture", "Async", "threads",
-    "get_results_generator", "run_after_async", "tPool"
+    "Pool",
+    "ProcessPool",
+    "NewFuture",
+    "Async",
+    "threads",
+    "get_results_generator",
+    "run_after_async",
+    "tPool",
 ]
