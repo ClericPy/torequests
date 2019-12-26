6c320d74d4371cdac0d2c2284f869125d0f2c461
clericpy <lidongone@qq.com>
2019-12-26 17:28:47 +0800

    fix runtime error for running loop

--- torequests/dummy.py
+++ torequests/dummy.py
@@ -629,11 +629,11 @@ class Requests(Loop):
 
     def __del__(self):
         if not self._closed:
-            NewTask(self.close(), loop=self.loop).x
+            asyncio.ensure_future(self.close())
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args):
         if not self._closed:
-            NewTask(self.close(), loop=self.loop).x
+            asyncio.ensure_future(self.close())
