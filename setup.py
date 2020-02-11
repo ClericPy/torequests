f2d1f870c519e0e31ee142b0098c314dfdab5343
clericpy <lidongone@qq.com>
2020-02-11 12:30:29 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -58,6 +58,19 @@ setup(
             'PySocks>=1.5.6, !=1.5.7', 'psutil', 'fuzzywuzzy', 'pyperclip'
         ],
     },
+    python_requires=
+    ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "",
+    ],
     author="ClericPy",
     author_email="clericpy@gmail.com",
     url="https://github.com/ClericPy/torequests",
