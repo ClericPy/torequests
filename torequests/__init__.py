caf1d7208d505d05a43814542de0505258143b98
clericpy <lidongone@qq.com>
2020-02-13 10:00:55 +0800

    update

--- torequests/__init__.py
+++ torequests/__init__.py
@@ -1,15 +1,12 @@
 #! coding: utf-8
 
-from .main import *
+from .main import (Async, NewFuture, Pool, ProcessPool, delete,
+                   disable_warnings, get, get_results_generator, head, options,
+                   patch, post, put, request, run_after_async, threads, tPool)
 
 __all__ = [
-    "Pool",
-    "ProcessPool",
-    "NewFuture",
-    "Async",
-    "threads",
-    "get_results_generator",
-    "run_after_async",
-    "tPool",
+    "Pool", "ProcessPool", "NewFuture", "Async", "threads",
+    "get_results_generator", "run_after_async", "tPool", "get", "post",
+    "options", "delete", "put", "head", "patch", "request", "disable_warnings"
 ]
-__version__ = '4.9.3'
+__version__ = '4.9.4'
