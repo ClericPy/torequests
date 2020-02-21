fb476f2ed51bcef42434aa82869d4b16eb13b7c4
clericpy <lidongone@qq.com>
2020-02-21 13:42:47 +0800

    fix #47

--- torequests/__init__.py
+++ torequests/__init__.py
@@ -9,4 +9,4 @@ __all__ = [
     "get_results_generator", "run_after_async", "tPool", "get", "post",
     "options", "delete", "put", "head", "patch", "request", "disable_warnings"
 ]
-__version__ = '4.9.5'
+__version__ = '4.9.6'
