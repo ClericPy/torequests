c10105148370ed6e0339fb92a873482b28affc60
clericpy <clericpy@gmail.com>
2020-04-14 23:03:37 +0800

    remove freq init.py

--- torequests/frequency_controller/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .sync_tools import Frequency
-
-try:
-    from .async_tools import Frequency as AsyncFrequency
-except SyntaxError:
-    # not support python2
-    AsyncFrequency = None
