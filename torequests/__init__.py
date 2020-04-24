e20f8d45e1caf2d60f6a772dee693367a51ab339
clericpy <clericpy@gmail.com>
2020-04-24 10:45:35 +0800

    5.0.1

--- torequests/__init__.py
+++ torequests/__init__.py
@@ -9,5 +9,5 @@ __all__ = [
     "get_results_generator", "run_after_async", "tPool", "get", "post",
     "options", "delete", "put", "head", "patch", "request", "disable_warnings"
 ]
-__version__ = '5.0.0'
+__version__ = '5.0.1'
 logging.getLogger("torequests").addHandler(logging.NullHandler())
