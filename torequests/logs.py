3debaf6bb5943aa47c869f0662cacb5b4e968fb7
clericpy <clericpy@gmail.com>
2020-06-15 23:53:22 +0800

    more features for init_logger

--- torequests/logs.py
+++ torequests/logs.py
@@ -1,5 +1,6 @@
 #! coding:utf-8
 import logging
+import logging.handlers
 import os
 import sys
 
@@ -9,33 +10,79 @@ if PY3:
     unicode = str
 
 
-def init_logger(
-        name="",
-        handler_path_levels=None,
-        level=logging.INFO,
-        formatter=None,
-        formatter_str=None,
-        datefmt="%Y-%m-%d %H:%M:%S",
-):
+def init_logger(name="",
+                handler_path_levels=None,
+                level=logging.INFO,
+                formatter=None,
+                formatter_str=None,
+                datefmt="%Y-%m-%d %H:%M:%S",
+                encoding="utf-8",
+                file_handler_class=None,
+                **file_handler_kwargs):
     """Add a default handler for logger.
 
-    Args:
+    Args::
 
-    name = '' or logger obj.
+        name = '' or logger obj.
 
-    handler_path_levels = [['loggerfile.log',13],['','DEBUG'],['','info'],['','notSet']] # [[path,level]]
+        handler_path_levels = [['loggerfile.log',13],['','DEBUG'],['','info'],['','notSet']] # [[path,level]]
 
-    level = the least level for the logger.
+        level = the least level for the logger.
 
-    formatter = logging.Formatter(
-            '%(levelname)-7s %(asctime)s %(name)s (%(filename)s: %(lineno)s): %(message)s',
-             "%Y-%m-%d %H:%M:%S")
+        formatter = logging.Formatter(
+                '%(levelname)-7s %(asctime)s %(name)s (%(filename)s: %(lineno)s): %(message)s',
+                "%Y-%m-%d %H:%M:%S")
 
-    formatter_str = '%(levelname)-7s %(asctime)s  %(name)s (%(funcName)s: %(lineno)s): %(message)s'
+        formatter_str = '%(levelname)-7s %(asctime)s  %(name)s (%(funcName)s: %(lineno)s): %(message)s'
 
-    custom formatter:
-        %(asctime)s  %(created)f  %(filename)s  %(funcName)s  %(levelname)s  %(levelno)s  %(lineno)s   %(message)s   %(module)s    %(name)s   %(pathname)s   %(process)s   %(relativeCreated)s   %(thread)s  %(threadName)s  
+        custom formatter:
+            %(asctime)s  %(created)f  %(filename)s  %(funcName)s  %(levelname)s  %(levelno)s  %(lineno)s   %(message)s   %(module)s    %(name)s   %(pathname)s   %(process)s   %(relativeCreated)s   %(thread)s  %(threadName)s
+
+        encoding: encoding for FileHandlers
+        file_handler_class: FileHandlers / RotatingFileHandler / TimedRotatingFileHandler, default to FileHandlers.
+        file_handler_kwargs: init kwargs for file_handler_class, such as when / interval / backupCount / maxBytes.
+
+    Demo::
+
+        # RotatingFileHandler
+        logger = init_logger(
+            '',
+            [['info.log', 'info']],
+            file_handler_class='RotatingFileHandler',
+            maxBytes=10 * 1024 * 1024,
+            backupCount=2,
+        )
+        logger.info('test')
+        logger.info('test')
+        # RotatingFileHandler
+        logger = init_logger(
+            '',
+            [['info.log', 'info']],
+            file_handler_class='TimedRotatingFileHandler',
+            when='D',
+            interval=1,
+            backupCount=7,
+        )
+        logger.info('test')
+        logger.info('test')
+
     """
+    if file_handler_class is None:
+        file_handler_class = logging.FileHandler
+    elif isinstance(file_handler_class, unicode):
+        alias_names = {
+            'size': 'RotatingFileHandler',
+            'time': 'TimedRotatingFileHandler',
+        }
+        file_handler_class = alias_names.get(file_handler_class,
+                                             file_handler_class)
+        file_handler_class = getattr(
+            logging, file_handler_class,
+            getattr(logging.handlers, file_handler_class))
+    if not file_handler_class:
+        raise NameError('invalid file_handler_class %s' % file_handler_class)
+    if 'encoding' in file_handler_kwargs:
+        encoding = file_handler_kwargs.pop('encoding')
     levels = {
         "NOTSET": logging.NOTSET,
         "DEBUG": logging.DEBUG,
@@ -57,10 +104,15 @@ def init_logger(
     # ---------------------------------------
     for each_handler in handler_path_levels:
         path, handler_level = each_handler
-        handler = logging.FileHandler(path) if path else logging.StreamHandler()
-        handler.setLevel(
-            levels.get(handler_level.upper(), 1
-                      ) if isinstance(handler_level, str) else handler_level)
+        if path:
+            handler = file_handler_class(path,
+                                         encoding=encoding,
+                                         **file_handler_kwargs)
+        else:
+            handler = logging.StreamHandler()
+        _level = levels.get(handler_level.upper(), 1) if isinstance(
+            handler_level, str) else handler_level
+        handler.setLevel(_level)
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     return logger
@@ -94,8 +146,8 @@ def print_info(*messages, **kwargs):
     ln = frame.f_lineno
     _file = frame.f_globals.get("__file__", "")
     fn = os.path.split(_file)[-1]
-    return print_logger.info(
-        sep.join(map(unicode, messages)), extra={
-            "ln": ln,
-            "fn": fn
-        })
+    return print_logger.info(sep.join(map(unicode, messages)),
+                             extra={
+                                 "ln": ln,
+                                 "fn": fn
+                             })
