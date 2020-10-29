1dfa7207161d3b3dee57c06e1f373eb4b56a2416
clericpy <clericpy@gmail.com>
2020-10-29 22:29:21 +0800

    support bool(find_one(xxx))

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -411,6 +411,8 @@ def test_find_one():
     assert find_one("a(B)", string, flags=re.I | re.DOTALL)[1] == "b"
     assert (find_one("a(B)", string)[1] or "default") == "default"
     assert (find_one("a(B)", string)[1]) == ""
+    assert find_one("a.*", string)
+    assert not find_one("a(B)", string)
     register_re_findone()
     assert re.findone("a(.)", string)[1] == "b"
 
